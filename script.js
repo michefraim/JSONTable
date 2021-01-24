32ed1fe14a892a5e46477e08f4646858de5c161a
michefraim <michefraim@gmail.com>
2021-01-24 16:03:01 +0200

    Assingment

--- script.js
+++ script.js
@@ -17,12 +17,83 @@ function fPercents(tGiven, tFinished) { // calculates finished tasks in percents
 }
 
 function niceDate(date) {
-    return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} - `
+    return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} at `
         + `${date.getHours()}:${date.getMinutes()}0`;
 }
 
 function miliTime(fTime, sTime) {
     return fTime - sTime;
+}
+
+function formTable() {
+    const body = document.body;
+    const table = document.createElement('table');
+    body.append(table);
+}
+
+function tableHead() {
+    const tr = document.createElement('tr');
+    document.querySelector('table').append(tr);
+    const titles = ["Topic:", "Start Time:", "Finish Time:", "Time Elapsed:", "Tasks Given", "Tasks Finished:", "% of finished:"];
+    for (let i = 0; i < titles.length; i++) {
+        const th = document.createElement('th');
+        th.innerText = titles[i];
+        tr.append(th);
+    }
+}
+
+function tableContent(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        const tr = document.createElement('tr');
+        document.querySelector('table').append(tr);
+
+        for (let prop in arr[i]) {
+            if (Object.prototype.hasOwnProperty.call(arr[i], prop)) {
+                const td = document.createElement('td');
+                if (prop === "startTime" || prop === "finishTime") { //start time and finish time 
+                    td.innerText = niceDate(arr[i][prop]);
+                    tr.append(td);
+                } else if (prop === "tTotal") {
+                    const x = miliTime(arr[i].finishTime, arr[i].startTime);
+                    if (x <= 18000000) {
+                        td.innerText = arr[i][prop];
+                        td.classList.add('lowestTime');
+                        tr.append(td);
+
+                    } else if (x <= 220000000) {
+                        td.innerText = arr[i][prop];
+                        td.classList.add('middleTime');
+                        tr.append(td);
+                    } else {
+                        td.innerText = arr[i][prop];
+                        td.classList.add('highestTime');
+                        tr.append(td);
+                    }
+
+                } else if (prop === "finishedPercents") {
+                    
+                    if ((fPercents(arr[i].tasksGiven, arr[i].tasksFinished) <= 50)) {
+                        td.innerText = arr[i][prop];
+                        td.classList.add('lowestPercents');
+                        tr.append(td);
+                    } else if ((fPercents(arr[i].tasksGiven, arr[i].tasksFinished) <= 75)) {
+                        td.innerText = arr[i][prop];
+                        td.classList.add('middlePercents');
+                        tr.append(td);
+                    }
+                    else {
+                        td.innerText = arr[i][prop];
+                        td.classList.add('highestPercents');
+                        tr.append(td);
+                    }
+                }
+                else {
+                    td.innerText = arr[i][prop]; 
+                    tr.append(td);
+                }
+            }
+        }
+    }
 }
 
 const tasks = [
@@ -114,49 +185,6 @@ for (let i = 0; i < tasks.length; i++) {
     tasks[i].finishedPercents = fPercents(tasks[i].tasksGiven, tasks[i].tasksFinished) + "%";
 }
 
-const tableHead = '<tr><th>Topic:</th><th>Start Time:</th><th>Finish Time:</th><th>Time Elapsed:</th>'
-    + '<th>Tasks Given:</th><th>Tasks Finished:</th><th>Finished Percents:</th></tr>';
-
-const rowsArray = ["", "", "", "", "", "", ""];
-
-for (let i = 0; i < rowsArray.length; i++) {
-    rowsArray[i] += "<tr>";
-    for (let prop in tasks[i]) {
-        if (Object.prototype.hasOwnProperty.call(tasks[i], prop)) {
-            if (prop === "startTime" || prop === "finishTime") { //start time and finish time 
-                rowsArray[i] += `<td> ${niceDate(tasks[i][prop])} </td> `;
-            } else if (prop === "tTotal") {
-                const x = miliTime(tasks[i].finishTime, tasks[i].startTime);
-                if (x <= 18000000) {
-                    rowsArray[i] += `<td class="lowestTime">  ${tasks[i][prop]}</td>`;
-
-                } else if (x <= 220000000) {
-                    rowsArray[i] += `<td class="middleTime">  ${tasks[i][prop]}</td>`;
-                } else {
-                    rowsArray[i] += `<td class="highestTime">  ${tasks[i][prop]}</td>`;
-                }
-            } else if (prop === "finishedPercents") {
-                if ((fPercents(tasks[i].tasksGiven, tasks[i].tasksFinished) <= 50)) {
-                    rowsArray[i] += `<td class="lowestPercents">  ${tasks[i][prop]}</td>`;
-
-                } else if ((fPercents(tasks[i].tasksGiven, tasks[i].tasksFinished) <= 75)) {
-                    rowsArray[i] += `<td class="middlePercents">  ${tasks[i][prop]}</td>`;
-                }
-                else {
-                    rowsArray[i] += `<td class="highestPercents">  ${tasks[i][prop]}</td>`;
-                }
-            }
-            else {
-                rowsArray[i] += `<td> ${tasks[i][prop]} </td>`;
-            }
-        }
-    }
-    rowsArray[i] += "</tr>";
-}
-
-let finalTable = `<table>${tableHead}`;
-for (let i = 0; i < rowsArray.length; i++) {
-    finalTable += rowsArray[i];
-}
-finalTable += "</table>";
-document.write(finalTable);
+formTable(); //forms new table in the body
+tableHead(); //adds the table headers
+tableContent(tasks); //adds the content of the table
